7d4aed7f9ee2897b6bc385ecddd5a46d634e2548
桐間紗路 <chikorita157@chikorita157.com>
2017-05-31 17:29:44 -0700

    Initial Commit

---
+++ AniLibrary Sync/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AniLibrary Sync
+//
+//  Created by 桐間紗路 on 2017/05/31.
+//  Copyright © 2017 Atelier Shiori. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
